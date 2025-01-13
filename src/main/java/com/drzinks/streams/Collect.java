0e7a52270179a40a1dfe21337b76306b2883279a
drzinks <you@example.com>
2025-01-13 19:25:59 +0100

    collecting to a map

---
+++ src/main/java/com/drzinks/streams/Collect.java
@@ -0,0 +1,33 @@
+package com.drzinks.streams;
+
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Collect {
+
+    public static void main(String[] args) {
+        StringBuilder word = Stream.of("ad", "jud", "i", "cate")
+                .collect(() -> new StringBuilder(), //supplier - StringBuilder::new
+                        (sb, str) -> sb.append(str),
+                        (sb1,sb2) -> sb1.append(sb2)
+                        );
+
+
+        List<String> shoppingList = Stream.of("cheese", "bacon", "eggs", "tortillas","onion").collect(Collectors.toList());
+        String shopping = shoppingList.stream().collect(Collectors.joining(","));
+        Double avg = shoppingList.stream().collect(Collectors.averagingInt(text -> text.length()));
+        Map<Integer, String> map = shoppingList.stream().collect(
+            Collectors.toMap(s -> s.length(), //how to obtain key
+                    s -> s, //how to obtain value
+                    (s1,s2) -> s1 + "," + s2, //optional: what if duplicate keys
+                    () -> new TreeMap<>() //result must be a TreeMap
+                ));
+        map.entrySet().forEach(System.out::println);
+
+
+    }
+
+}
